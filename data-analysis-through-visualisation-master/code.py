f0b9091d4de036122ac9993b8912f9723b2daebc
SetuCoder <58362876+SetuCoder@users.noreply.github.com>
2022-01-11 16:09:50 +0530

    Delete data-analysis-through-visualisation-master directory

--- data-analysis-through-visualisation-master/code.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import csv
-import plotly.express as px
-df = pd.read_csv("data.csv")
-mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
-fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt")
-fig.show()
